1125864d7492508f999ce7a0baa7a7680b0ff356
Konstantin Morenko <me@konstantin-morenko.ru>
2018-04-15 08:50:40 +0300

    Initial commit

---
+++ pomodoro.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+source timer-source.sh
+
+WORK=0
+SHORT=0
+LONG=0
+
+function save {
+    echo -e "WORK=$WORK\nSHORT=$SHORT\nLONG=$LONG\n" > $DBASE_FILE
+}
+
+# ====================
+function load {
+    DBASE_FILE=$1
+    if [ -a $DBASE_FILE ]; then
+	source $DBASE_FILE
+    else
+	touch $DBASE_FILE
+	save
+    fi
+    return
+}
+# ====================
+#source $DBASE_FILE
+load `date +%F`.pom
+
+echo "Pomodoro timer!"
+echo "==============="
+
+TIME_WORK=20
+TIME_SHORT=5
+TIME_LONG=30
+
+ANSWER=""
+UNINT_WORK=0
+while [[ $ANSWER != "q" ]]; do
+    echo "--------------------"
+    echo "w - work ($TIME_WORK) [$UNINT_WORK; $WORK]"
+    echo "s - short break ($TIME_SHORT) [$SHORT]"
+    echo "l - long break ($TIME_LONG) [$LONG]"
+    echo "q - quit"
+    read -p "? " ANSWER INTERVAL
+    case $ANSWER in
+	w*) timer ${INTERVAL:-$TIME_WORK}
+	    $(( WORK+=1 ))
+	    $(( UNINT_WORK+=1 ))
+	    save ;;
+	s*) timer ${INTERVAL:-$TIME_SHORT} 0 "Time to work! (x)"
+	    $(( SHORT+=1 ))
+	    save ;;
+	l*) timer ${INTERVAL:-$TIME_LONG} 5 "Time to work! (x)"
+	    $(( LONG+=1 ))
+	    UNINT_WORK=0
+	    save ;;
+	q*) save ;;
+    esac
+done
---
+++ timer-source.sh
@@ -0,0 +1,45 @@
+function notify {
+    MESSAGE=$1
+    EXPIRE=$2
+    NOTIFIER="notify-send"
+    case $EXPIRE in
+	pin)
+	    $NOTIFIER "$MESSAGE" --expire-time=0 ;;
+	*)
+	    $NOTIFIER "$MESSAGE" ;;
+    esac
+    return
+}
+
+function message {
+    MESSAGE=$1
+    echo -ne "\r                    " # clear string
+    echo -ne "\r$MESSAGE"
+    return
+}
+
+function output_left {
+    LEFT=$1
+    NOTIF_INTERVAL=$2
+    MESSAGE="$LEFT minutes left"
+    message "$MESSAGE"
+    if [ $NOTIF_INTERVAL != 0 ] && [ $(($LEFT % $NOTIF_INTERVAL)) == 0 ]; then
+	notify "$MESSAGE"
+    fi
+    return
+}
+
+function timer {
+    INTERVAL=$1
+    NOTIF_INTERVAL=${2:-5}
+    FINAL_MESSAGE=${3:-"Take a break! (x)"}
+    notify "Started!"
+    for LEFT in $(seq $INTERVAL -1 1); do
+	output_left $LEFT $NOTIF_INTERVAL
+	sleep 60
+    done
+    message "Total $INTERVAL minutes"
+    echo ""
+    notify "$FINAL_MESSAGE" pin
+    return
+}
