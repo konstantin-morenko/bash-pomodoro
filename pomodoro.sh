6c94fb4c9f18f38d4bf96f5926c126cd1a24b396
Konstantin Morenko <me@konstantin-morenko.ru>
2018-04-16 19:54:25 +0300

    Counting time intervals

--- pomodoro.sh
+++ pomodoro.sh
@@ -11,9 +11,14 @@ WORK_INTERVAL=20
 SHORT_INTERVAL=5
 LONG_INTERVAL=30
 
+WORK_TIME=0
+SHORT_TIME=0
+LONG_TIME=0
+
 function save {
     echo -e "WORK_NUM=$WORK_NUM\nSHORT_NUM=$SHORT_NUM\nLONG_NUM=$LONG_NUM\nWORK_NUM_UNINT=$WORK_NUM_UNINT\n" > $DBASE_FILE
     echo -e "WORK_INTERVAL=$WORK_INTERVAL\nSHORT_INTERVAL=$SHORT_INTERVAL\nLONG_INTERVAL=$LONG_INTERVAL\n" >> $DBASE_FILE
+    echo -e "WORK_TIME=$WORK_TIME\nSHORT_TIME=$SHORT_TIME\nLONG_TIME=$LONG_TIME\n" >> $DBASE_FILE
 }
 
 function load {
@@ -35,26 +40,29 @@ ANSWER=""
 
 while [[ $ANSWER != "q" ]]; do
     echo "--------------------"
-    printf "%-1s - %-12s {%2d} [%2d, %2d] (%3d)\n" "w" "work"  $WORK_INTERVAL $WORK_NUM_UNINT $WORK_NUM $(($WORK_INTERVAL*$WORK_NUM))
-    printf "%-1s - %-12s {%2d}     [%2d] (%3d)\n" "s" "short break" $SHORT_INTERVAL $SHORT_NUM $(($SHORT_INTERVAL*$SHORT_NUM))
-    printf "%-1s - %-12s {%2d}     [%2d] (%3d)\n" "l" "long break" $LONG_INTERVAL $LONG_NUM $(($LONG_INTERVAL*$LONG_NUM))
+    printf "%-1s - %-12s {%2d} [%2d, %2d] (%3d)\n" "w" "work"  $WORK_INTERVAL $WORK_NUM_UNINT $WORK_NUM $WORK_TIME
+    printf "%-1s - %-12s {%2d}     [%2d] (%3d)\n" "s" "short break" $SHORT_INTERVAL $SHORT_NUM $SHORT_TIME
+    printf "%-1s - %-12s {%2d}     [%2d] (%3d)\n" "l" "long break" $LONG_INTERVAL $LONG_NUM $LONG_TIME
     echo "q - quit"
     read -p "? " ANSWER INTERVAL
     case $ANSWER in
-	w) timer ${INTERVAL:-$WORK_INTERVAL}
+	w) timer ${INTERVAL:=$WORK_INTERVAL}
 	   WORK_NUM=$(( $WORK_NUM+1 ))
 	   WORK_NUM_UNINT=$(( $WORK_NUM_UNINT+1 ))
+	   WORK_TIME=$(( $WORK_TIME+$INTERVAL ))
 	   save ;;
 	W) WORK_INTERVAL=$INTERVAL
 	   save ;;
-	s) timer ${INTERVAL:-$SHORT_INTERVAL} 0 "Time to work! (x)"
+	s) timer ${INTERVAL:=$SHORT_INTERVAL} 0 "Time to work! (x)"
 	   SHORT_NUM=$(( $SHORT_NUM+1 ))
+	   SHORT_TIME=$(( $SHORT_TIME+$INTERVAL ))
 	   save ;;
 	S) SHORT_INTERVAL=$INTERVAL
 	   save ;;
-	l) timer ${INTERVAL:-$LONG_INTERVAL} 5 "Time to work! (x)"
+	l) timer ${INTERVAL:=$LONG_INTERVAL} 5 "Time to work! (x)"
 	   LONG_NUM=$(( $LONG_NUM+1 ))
 	   WORK_NUM_UNINT=0
+	   LONG_TIME=$(( $LONG_TIME+$INTERVAL ))
 	   save ;;
 	L) LONG_INTERVAL=$INTERVAL
 	   save ;;
